5180a4f40f3cbd4f5525cb567ae7a11083b528d9
Anindito Rahardyaksa <ditorahard@gmail.com>
2019-01-12 17:50:59 +0700

    change subheader color to red

--- app/components/Screens/Home/Home.component.style.js
+++ app/components/Screens/Home/Home.component.style.js
@@ -1,8 +1,12 @@
 import {StyleSheet} from 'react-native';
+import SCREEN_IMPORT from 'Dimensions';
+  
+const SCREEN_HEIGHT = SCREEN_IMPORT.get('window').height;
 
 export default StyleSheet.create({
 	container: {
 	    padding:20,
+	    height:SCREEN_HEIGHT
 	},
 	imageModal:{
 		flex:1,
--- app/components/Subheader/Subheader.component.style.js
+++ app/components/Subheader/Subheader.component.style.js
@@ -1,13 +1,11 @@
 import {StyleSheet} from 'react-native';
-// import theme from '../../styles/theme.style';
-// import {headingText, textInput} from '../../styles/common.style';
 
 export default StyleSheet.create({
 	hero:{
 		paddingTop:20,
 		paddingBottom:20,
 		alignItems:'center',
-		backgroundColor:'#014DB2',
+		backgroundColor:'#D20202',
 	},
 	whitetext:{
 		color:'#FFFFFF',
